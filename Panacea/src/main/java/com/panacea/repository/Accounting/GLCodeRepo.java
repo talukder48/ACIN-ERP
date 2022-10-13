6b8870a174a438c58715d4e97c0c006fde160a38
Mosharraf Hossain Talukder <talukder.csedu@gmail.com>
2022-10-14 01:41:13 +0600

    Accounting Part partially Done.

--- Panacea/src/main/java/com/panacea/repository/Accounting/GLCodeRepo.java
+++ Panacea/src/main/java/com/panacea/repository/Accounting/GLCodeRepo.java
@@ -10,6 +10,9 @@ public interface GLCodeRepo extends JpaRepository <GLCode,String> {
 	@Query(value = "SELECT * FROM `as_glcode` where `gl_level`='L'", nativeQuery = true)
 	 List<GLCode> FindTransactionGL();
 	
+	@Query(value = "SELECT * FROM `as_glcode` where `gl_level`='P'", nativeQuery = true)
+	 List<GLCode> FindPrimeGL();
+	
 	@Query(value = "SELECT `gl_name` FROM `as_glcode` WHERE `gl_code`=?1", nativeQuery = true)
 	 String TransactionHead(String TransactionHead);
 	
